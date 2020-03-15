363935a81430a4b93f7578f2e99488adfd945356
KOremi(AntennaJane) <antennajane@yahoo.co.jp>
2020-03-15 21:40:49 +0900

    dump statuses/user_timeline

--- src/exec.php
+++ src/exec.php
@@ -7,6 +7,15 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 $ini = parse_ini_file(__DIR__ . "/../app/parameters.ini");
 
 $connection = new TwitterOAuth($ini["CONSUMER_KEY"], $ini["CONSUMER_SECRET"], $ini["ACCESS_TOKEN"], $ini["ACCESS_TOKEN_SECRET"]);
-$content = $connection->get("account/verify_credentials");
+$contents = $connection->get("statuses/user_timeline", [
+    "include_rts" => false,
+    "trim_user" => true,
+    "user_id" => $ini["USER_ID"],
+]);
 
-var_dump($content);
+foreach (array_reverse($contents) as $content) {
+    echo $content->id;
+    echo PHP_EOL;
+    echo $content->text;
+    echo PHP_EOL;
+}
