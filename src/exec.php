ea51a8285b67d3ce2f1655295519b30b1fa1acda
KOremi(AntennaJane) <antennajane@yahoo.co.jp>
2020-03-15 21:45:22 +0900

    post statuses/update

--- src/exec.php
+++ src/exec.php
@@ -7,15 +7,20 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 $ini = parse_ini_file(__DIR__ . "/../app/parameters.ini");
 
 $connection = new TwitterOAuth($ini["CONSUMER_KEY"], $ini["CONSUMER_SECRET"], $ini["ACCESS_TOKEN"], $ini["ACCESS_TOKEN_SECRET"]);
-$contents = $connection->get("statuses/user_timeline", [
-    "include_rts" => false,
-    "trim_user" => true,
-    "user_id" => $ini["USER_ID"],
-]);
+//$contents = $connection->get("statuses/user_timeline", [
+//    "include_rts" => false,
+//    "trim_user" => true,
+//    "user_id" => $ini["USER_ID"],
+//]);
+//
+//foreach (array_reverse($contents) as $content) {
+//    echo $content->id;
+//    echo PHP_EOL;
+//    echo $content->text;
+//    echo PHP_EOL;
+//}
 
-foreach (array_reverse($contents) as $content) {
-    echo $content->id;
-    echo PHP_EOL;
-    echo $content->text;
-    echo PHP_EOL;
-}
+$connection->post("statuses/update", [
+    "status" => "テストツイート成功……",
+    "trim_user" => true,
+]);
